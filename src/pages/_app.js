e39c30a30f5e85a99b439cee8e7da959d5e55fa0
OGONO <129948297+OGONO@users.noreply.github.com>
2024-03-23 00:33:14 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,15 +6,18 @@ import "../../styles/globals.scss";
 export default function App({ Component, pageProps }) {
   return (
     <>
-<!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-ETL1YQLSQB"></script>
-  <script>
-   window.dataLayer = window.dataLayer || [];
-   function gtag(){dataLayer.push(arguments);}
-   gtag('js', new Date());
-
-   gtag('config', 'G-ETL1YQLSQB');
-</script>
+  <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id =G-ETL1YQLSQB"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.datalayer = window.datalayer || [];
+            function gtag(){datalayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config' , 'G-ETL1YQLSQB');
+         `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
