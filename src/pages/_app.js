22a2e722f0c913a7e7fc36e02589de1dfe2613d9
OGONO <129948297+OGONO@users.noreply.github.com>
2024-03-23 00:07:25 +0200

    adding google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,24 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
+import Script from "next/script";
 
-
-import '../../styles/globals.scss'
+import "../../styles/globals.scss";
 
 export default function App({ Component, pageProps }) {
-  return  <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id =G-ETL1YQLSQB"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.datalayer = window.datalayer || [];
+            function gtag(){datalayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config' , 'G-ETL1YQLSQB');
+         `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
