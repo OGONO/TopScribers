88a1829a8aab2b45221908ab3355f30f1dd7074b
OGONO <129948297+OGONO@users.noreply.github.com>
2024-03-24 12:21:57 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import head from "next/head"
+import Head from "next/head"
 import PostCard from "@/components/PostCard";
 import { getPosts } from "../../services"
 import HeroSection from "../../src/components/HeroSection";
@@ -16,9 +16,9 @@ export default function Home({ posts }) {
   return (
   
     <main className='flex min-h-screen flex-col bg-white' >
-    <head>
+     <Head>
         <meta name="google-adsense-account" content="ca-pub-8382537736577233">
-      </head>
+      </Head>
 
     <Navbar/>
     <br></br>
