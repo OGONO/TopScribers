74decb0b312c57264d839d76e4eaac6f5526b6de
OGONO <129948297+OGONO@users.noreply.github.com>
2024-03-24 12:06:30 +0200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-3624894722008525">
+     </Head>
       <body>
         <Main />
         <NextScript />
