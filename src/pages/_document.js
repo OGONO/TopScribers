7333522f17bd0254768a241ec08e45591d179986
OGONO <129948297+OGONO@users.noreply.github.com>
2024-03-24 12:08:01 +0200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <meta name="google-adsense-account" content="ca-pub-3624894722008525">
-     </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="google-adsense-account" content="ca-pub-3624894722008525">
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
+
